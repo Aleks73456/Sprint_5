49459986fd4ae6ec20daf93e0c5a067dcf2e1fb3
Aleksandr <SashaShr7@yandex.ru>
2025-08-13 10:11:32 +0400

    Добавил явные ожидания

--- tests/user_login.py
+++ tests/user_login.py
@@ -1,33 +1,35 @@
-import time
 from locators import Locators
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 
 class TestLoginPage:
     
     def test_user_login(self, driver):
         driver.get("https://qa-desk.stand.praktikum-services.ru")
-        driver.find_element(*Locators.login_button).click()
-        time.sleep(1)
+        driver.find_element(*Locators.login_button).click()   
+        WebDriverWait(driver, 1).until(expected_conditions.visibility_of_element_located(Locators.imput_login_email))
         driver.find_element(*Locators.imput_login_email).send_keys("aser@mail.ru")
-        time.sleep(1)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.imput_login_password))
         driver.find_element(*Locators.imput_login_password).send_keys("2846")
-        time.sleep(1)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.login))
         driver.find_element(*Locators.login).click()
-        time.sleep(5)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.user_avatar))
         assert driver.find_element(*Locators.user_avatar).is_displayed() and "User." in driver.find_element(*Locators.user_name).text
 
     
     def test_user_logout(self, driver):
         driver.get("https://qa-desk.stand.praktikum-services.ru")
         driver.find_element(*Locators.login_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 1).until(expected_conditions.visibility_of_element_located(Locators.imput_login_email))
         driver.find_element(*Locators.imput_login_email).send_keys("aser@mail.ru")
-        time.sleep(1)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.imput_login_password))
         driver.find_element(*Locators.imput_login_password).send_keys("2846")
-        time.sleep(1)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.login))
         driver.find_element(*Locators.login).click()
-        time.sleep(5)
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.exit_button))
         driver.find_element(*Locators.exit_button).click()
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.invisibility_of_element_located(Locators.user_avatar))
         assert driver.find_element(*Locators.login_button).is_displayed() and not driver.find_elements(*Locators.user_avatar) and not driver.find_elements(*Locators.user_name)
 
     
--- tests/user_registration.py
+++ tests/user_registration.py
@@ -2,6 +2,8 @@
 import time
 import random
 from locators import Locators
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class TestRegistrationPage:
 
@@ -13,49 +15,49 @@ class TestRegistrationPage:
         email = TestRegistrationPage.generate_email(self)
         driver.get("https://qa-desk.stand.praktikum-services.ru")
         driver.find_element(*Locators.login_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.no_account_button))
         driver.find_element(*Locators.no_account_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_email))
         driver.find_element(*Locators.imput_email).send_keys(email)
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_password))
         driver.find_element(*Locators.imput_password).send_keys("2846")
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.confirm_password))
         driver.find_element(*Locators.confirm_password).send_keys("2846")
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.create_account_button))
         driver.find_element(*Locators.create_account_button).click()
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.user_avatar))
         assert driver.find_element(*Locators.user_avatar).is_displayed() and "User." in driver.find_element(*Locators.user_name).text
     
     def test_user_not_valid_email_registration(self, driver):
         driver.get("https://qa-desk.stand.praktikum-services.ru")
         driver.find_element(*Locators.login_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.no_account_button))
         driver.find_element(*Locators.no_account_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_email))
         driver.find_element(*Locators.imput_email).send_keys("aser5mail.ru")
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_password))
         driver.find_element(*Locators.imput_password).send_keys("2846")
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.confirm_password))
         driver.find_element(*Locators.confirm_password).send_keys("2846")
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.create_account_button))
         driver.find_element(*Locators.create_account_button).click()
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.text_error_email))
         assert "Ошибка" in  driver.find_element(*Locators.text_error_email).text and driver.find_element(*Locators.red_fields).is_displayed()
     
     def test_user_not_valid_password_registration(self, driver):
         email = TestRegistrationPage.generate_email(self) 
         driver.get("https://qa-desk.stand.praktikum-services.ru")
         driver.find_element(*Locators.login_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.no_account_button))
         driver.find_element(*Locators.no_account_button).click()
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_email))
         driver.find_element(*Locators.imput_email).send_keys(email)
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.imput_password))
         driver.find_element(*Locators.imput_password).send_keys("2846")
-        time.sleep(1)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.confirm_password))
         driver.find_element(*Locators.confirm_password).send_keys("28467")
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable(Locators.create_account_button))
         driver.find_element(*Locators.create_account_button).click()
-        time.sleep(5)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.text_error_password))
         assert "Пароли не совпадают" in  driver.find_element(*Locators.text_error_password).text and driver.find_element(*Locators.red_fields).is_displayed()
  
